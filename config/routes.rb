5165a44f4e882b235330755b454d6e36870222c3
216d0136 <keukemori0226@gmail.com>
2020-06-07 14:29:45 +0900

    max

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  get 'home/about'
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+
+  root to: 'home#top'
+
+end
